7a023387cc5b565055e585175fb607f34977cfa4
Annie Zheng <annie.zheng@manchester.ac.uk>
2025-10-10 16:48:26 +0100

    Add rocrates facet class and title property

--- src/server/sparql/rocrate/sparql_queries/SparqlQueriesRocrates.js
+++ src/server/sparql/rocrate/sparql_queries/SparqlQueriesRocrates.js
@@ -2,9 +2,9 @@ const perspectiveID = 'rocrates'
 
 export const rocrateProperties = `
     {
-      ?id skos:prefLabel ?prefLabel__id .
+      ?id ns11:name ?prefLabel__id .
       BIND(?prefLabel__id AS ?prefLabel__prefLabel)
-      BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+      BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)\\\\/ro_crate\\\\/$", "$1")) AS ?prefLabel__dataProviderUrl)
       BIND(?id as ?uri__id)
       BIND(?id as ?uri__dataProviderUrl)
       BIND(?id as ?uri__prefLabel)
