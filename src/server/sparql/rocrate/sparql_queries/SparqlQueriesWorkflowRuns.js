f5c55953178cd1f58213b85a1096e263f0aa49c1
Annie Zheng <annie.zheng@manchester.ac.uk>
2025-11-19 17:41:48 +0000

    Add workflow run properties

--- src/server/sparql/rocrate/sparql_queries/SparqlQueriesWorkflowRuns.js
+++ src/server/sparql/rocrate/sparql_queries/SparqlQueriesWorkflowRuns.js
@@ -9,4 +9,43 @@ export const workflowRunProperties = `
       BIND(?uri as ?uri__dataProviderUrl)
       BIND(?uri as ?uri__prefLabel)
     }
+    UNION
+    {
+      ?id owl:sameAs/ns11:agent ?agent__id .
+      ?agent__id ns11:name ?agent__prefLabel .
+      BIND(?agent__id AS ?agent__dataProviderUrl)
+    }
+    UNION
+    {
+      ?id owl:sameAs/ns11:startTime ?startTime__id .
+      BIND(CONCAT(STR(DAY(?startTime__id)), "/", STR(MONTH(?startTime__id)), "/", STR(YEAR(?startTime__id))) as ?startTime__prefLabel)
+    }
+    UNION
+    {
+      ?id owl:sameAs/ns11:endTime ?endTime__id .
+      BIND(CONCAT(STR(DAY(?endTime__id)), "/", STR(MONTH(?endTime__id)), "/", STR(YEAR(?endTime__id))) as ?endTime__prefLabel)
+    }
+    UNION
+    {
+      ?id owl:sameAs/ns11:instrument ?instrument__id .
+      ?instrument__id ns11:name ?instrument__prefLabel .
+      BIND(?instrument__id AS ?instrument__dataProviderUrl)
+    }
+    UNION
+    {
+      ?id owl:sameAs/ns11:object ?object__id .
+      ?object__id ns11:name ?object__prefLabel .
+      BIND(?object__id AS ?object__dataProviderUrl)
+    }
+    UNION
+    {
+      ?id owl:sameAs/ns11:result ?result__id .
+      ?result__id ns11:name ?result__prefLabel .
+      BIND(?result__id AS ?result__dataProviderUrl)
+    }
+    UNION
+    {
+      ?id owl:sameAs/ns11:description ?description__id .
+      BIND(?description__id AS ?description__prefLabel)
+    }
 `
